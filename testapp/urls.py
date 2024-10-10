2f6fd51b3b552816a48fb7fd64ef164cf809551e
Nandana <nandanas2109@gmail.com>
2024-10-10 13:12:24 +0530

    modified views and added templates

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,4 +2,5 @@ from django.urls import path #type:ignore
 from . import views
 urlpatterns=[
     path('sample/',views.sample,name='sample'),
+    path('page2/',views.page2),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render #type:ignore
 from django.http import HttpResponse #type:ignore
+from django.template import loader  #type:ignore
 def sample(request):
     msg="<h1>Sample django project</h1>"
     msg+="<p>This is my first django project in github </p>"
     return HttpResponse(msg)
+def page2(request):
+    tmp=loader.get_template('index.html')
+    return HttpResponse(tmp.render())
 
 # Create your views here.
